99cc042d559637770774c6fa7ae524fea129f86c
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-05-10 11:25:28 -0700

    adjust eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-	'env': {
-		'commonjs': true,
-		'es2021': true,
-		'node': true
-	},
-	'extends': 'eslint:recommended',
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        'jest': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
 }

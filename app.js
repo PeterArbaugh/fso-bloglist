c2fb41c49bf134d5c65f8a07728dc4778450f608
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-06-26 16:20:37 -0700

    exercise 4.18

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGODB_URI)
@@ -14,5 +15,6 @@ app.use(express.json())
 
 app.use('/api/blogs', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
   
 module.exports = app
